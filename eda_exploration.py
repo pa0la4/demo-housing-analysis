c8b7ffbf97762313db2241670fa2e8f8108d777c
pa0la4 <paolacancino29@gmail.com>
2026-02-09 17:59:01 -0600

    Revert "Delete eda_exploration.py"
    This reverts commit d7cbf6ebd15d4dd6c5bc52cd45067d63f88d6fcc.

---
+++ eda_exploration.py
@@ -0,0 +1,12 @@
+# Exploratory data analysis
+# Housing Price Prediction Project
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+## Load the data
+# df = pd.read_csv('data/housing.csv')
+
+# TODO: Add distribution plots and correlation heatmap
+print('EDA script intialized')
